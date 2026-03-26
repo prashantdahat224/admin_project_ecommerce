71e82fecf9294ba1a9f2a8d9ae990b498c8df02b
prashant dahat <prashantdahat224@gmail.com>
2026-03-26 13:11:31 -0700

    latest changes 14

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -10,13 +10,18 @@ const Account = () => {
 return (
 <div className="flex items-center justify-center min-h-screen">
 
-        <h1 className="text-xl mb-6">Welcome to Admin Panel of WISHMOS</h1>
+         <div>
+        <h1 className="text-lg mb-6">Welcome to Admin Panel of WISHMOS</h1>
 
 
 <button onClick={() => navigate("/AdminPassword")}
+
+
  className="bg-gray-500 text-white px-4 py-3 px-1 rounded hover:bg-gray-600">
-Click here
+Continue
 </button>
+
+</div>
 </div>
 );
 
